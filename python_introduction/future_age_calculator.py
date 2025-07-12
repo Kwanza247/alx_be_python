7cde83ba7692b286f0ef0c311c21f477547b29a7
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2025-07-12 13:25:28 +0100

    Fix input prompt to meet project requirements

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,8 +1,8 @@
 #prompt the user to input their age
-current_age = int(input("How old are you.? "))
+current_age = int(input("How old are you? "))
 
 # calculate how old the user would be in 2050 if current year is 2023
-future_age = 27 + current_age
+future_age = (2050 - 2023) + current_age
 
 #print user age in 2050
 print(f"In 2050, you will be {future_age} years old.")
